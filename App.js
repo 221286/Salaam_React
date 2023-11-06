d29277339db317dd47699846db69f88b6450fcb8
221286 <140632301+221286@users.noreply.github.com>
2023-11-06 12:08:59 +0530

    episode 3

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import  ReactDOM  from "react-dom/client";
 //JSX=>React element(object)=>html elemnt(render)
 
 const Jsxhead= ()=>(<h1 id="first" className="first1"> This is pehla Salaam</h1>); 
+const elem = <h1>This is element</h1>
 
 const Secondheadcomponent =()=>(<div>
 <Jsxhead />
+{Jsxhead()}
+{elem}
+<Jsxhead></Jsxhead>
 <h1 id="#second"> This is dusra Salaam
 </h1></div>);
 
