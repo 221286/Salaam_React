2f5ef998380ffa8efce50d0a9ac52888ebfabe78
221286 <140632301+221286@users.noreply.github.com>
2023-11-08 12:41:52 +0530

    Filter applied

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,23 @@
 import {RESTAURANTLIST} from "../utils/restaurant_mockdata.js";
 import Restcard from "./Restcard.js";
+import { useState } from "react";
 
 const Body = ()=>{
+
+    
+      const [getRestaurantlist,setRestaurantlist] = useState(RESTAURANTLIST);
     return (
         <div className="body-container">
             <div className="search-container">
-            Search space{RESTAURANTLIST.length}
+
+            <button onClick={()=>{
+                const filter_list=getRestaurantlist.filter((res)=>res.info.avgRating>4);
+                setRestaurantlist(filter_list);
+
+            }}>Filter</button>
             </div>
             <div className="restcard-container">
-            {RESTAURANTLIST.map((restra)=>(<Restcard key={restra.info.id} restaurant={restra}></Restcard>))}
+            {getRestaurantlist.map((restra)=>(<Restcard key={restra.info.id} restaurant={restra}></Restcard>))}
             </div>
         </div>
     )
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const CDN_CLOUDINARY_UI= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const HEADER_LOGO_UI= "https://pbs.twimg.com/profile_images/1093816939602862080/lpdhQMKs_400x400.jpg";
+export const HEADER_LOGO_UI= "https://images.yourstory.com/cs/images/companies/logosC141575978425306png?fm=auto&ar=1:1&mode=fill&fill=solid&fill-color=fff";
